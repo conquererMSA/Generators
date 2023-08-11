ded1aae2c21cbd0e57f23f27cee503f5b209a713
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-11 18:43:37 +0600

    Generator print by one one by next() method

--- easyGenerators.py
+++ easyGenerators.py
@@ -12,10 +12,36 @@ def myGenerator():
     yield"MSA"
     yield"Tata"
 
+'''myGenerator function er sob yield value memory the save hy na. 
+value print korar jonno next() method e call korle shudhu nirdisto 
+value save hoy and amra print() e dekhte pai.
+'''
 g=myGenerator()
-print(g) #genertor objct from class Generator
-print(next(g)) # Hello world
-print(next(g)) # MSA
+# print(g) #genertor objct from class Generator
+# print(next(g)) # Hello world
+# print(next(g)) # MSA
+#
 # print generator value by for loop
-for value in g:
-    print(value) # Tata
+# for value in g:
+#     print(value) # Tata
+    #sob gulu print hobe na karon next() method diye protom duita
+    # value print kora hoye geche.
+
+
+def countDown_Generator(num):
+    print("Start Countdown...")
+    while num>0:
+        yield num
+        num-=1
+nums=countDown_Generator(10)
+print(next(nums)) #10
+print(next(nums)) #9
+
+#for loop e 8-1 porzonto print hobe karon generator function last
+# state mone rakhe ebong arekbar call kora hole last exicution
+# zekhane theme geche tarpor theke ek ek kore value return kore
+print('.......')
+for i in nums:
+    print(i)
+
+
